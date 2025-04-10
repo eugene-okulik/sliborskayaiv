484129ac247bc1540880ec91c98dba7400c2dbc8
IrinaSli <ir.kasiakina@gmail.com>
2025-04-10 17:34:03 +0200

    Homework_3 done

---
+++ homework/irina_sliborskaya/Homework_3/task_four.py
@@ -0,0 +1,12 @@
+import math
+
+def count_hypotenuse(a, b):
+    hypotenuse = math.sqrt(a ** 2 + b ** 2)
+    print('Hypotenuse is', hypotenuse)
+
+def count_square(a, b):
+    square = a * b / 2
+    print('Square is', square)
+
+count_hypotenuse(2,5)
+count_square(4, 6)
