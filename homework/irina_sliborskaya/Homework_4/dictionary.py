45506609f891e834d9afc557b62fb200ed4cfb8f
IrinaSli <ir.kasiakina@gmail.com>
2025-04-11 18:11:24 +0200

    homework 4 done

--- homework/irina_sliborskaya/Homework_4/dictionary.py
+++ homework/irina_sliborskaya/Homework_4/dictionary.py
@@ -1,24 +1,22 @@
 my_dict = {
-        'list': ['Portugal', 'Spain', 'Denmark', 'Bahrain', 'Italy'],
-        'tuple': ('one', 2, True, 'four', 5, 2),
-        'dict': {'Apple': 'red', 'Lime': 'yellow', 'Pineapple': 'green',  'Carrot': 'orange', 'Tomato': 'dark red'},
-        'set': {1, 3, 5, 5, 9, 'one', 'one', True, False, 32}
-        }
+    'list': ['Portugal', 'Spain', 'Denmark', 'Bahrain', 'Italy'], 'tuple': ('one', 2, True, 'four', 5, 2),
+    'dict': {'Apple': 'red', 'Lime': 'yellow', 'Pineapple': 'green', 'Carrot': 'orange', 'Tomato': 'dark red'},
+    'set': {1, 3, 5, 5, 9, 'one', 'one', True, False, 32}}
 
-#print last item from "tuple"
+# print last item from "tuple"
 print(my_dict['tuple'][-1])
 
-#actions with 'list': add and remove element
+# actions with 'list': add and remove element
 my_dict['list'].append('Poland')
 my_dict['list'].pop(1)
 
-#actions with 'dict': add and remove
+# actions with 'dict': add and remove
 my_dict['dict']['i am a tuple'] = 'you are not'
 my_dict['dict'].pop('Lime')
 
-#action with 'set': add and remove
+# action with 'set': add and remove
 my_dict['set'].add(18)
 my_dict['set'].remove(5)
 
-#print the whole dictionary
+# print the whole dictionary
 print(my_dict)
