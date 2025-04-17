140af0ead42e5d66b31c911489901bf2826fc38c
IrinaSli <ir.kasiakina@gmail.com>
2025-04-17 09:17:23 +0200

    new option for task two

--- homework/irina_sliborskaya/Homework_5/task_two.py
+++ homework/irina_sliborskaya/Homework_5/task_two.py
@@ -1,28 +1,15 @@
-from unicodedata import digit
-
 text1 = 'результат операции: 42'
 text2 = 'результат операции: 514'
 text3 = 'результат работы программы: 9'
 
-# to determine indexes
-# print(text1.index(':'))
-# print(text3.index(':'))
+colon_index1 = text1.index(':')
+colon_index2 = text2.index(':')
+colon_index3 = text3.index(':')
 
-result1 = int(text1.split()[-1]) + 10
-result2 = int(text2.split()[-1]) + 10
-result3 = int(text3.split()[-1]) + 10
+result1 = int(text1[colon_index1 + 1:].strip()) + 10
+result2 = int(text2[colon_index2 + 1:].strip()) + 10
+result3 = int(text3[colon_index3 + 1:].strip()) + 10
 
 print(result1)
 print(result2)
 print(result3)
-
-# or another way
-# text_options = [
-#     'результат операции: 42',
-#     'результат операции: 514',
-#     'результат работы программы: 9'
-# ]
-#
-# for options in text_options:
-#     result = int(options.split(':')[1].strip()) + 10
-#     print(result)
