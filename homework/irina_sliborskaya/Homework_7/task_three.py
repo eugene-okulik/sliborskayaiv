bd74c0dd64a60c2a38ad43f56c84f87b595c6493
IrinaSli <ir.kasiakina@gmail.com>
2025-04-21 12:43:30 +0200

    homework 7 is done

--- homework/irina_sliborskaya/Homework_7/task_three.py
+++ homework/irina_sliborskaya/Homework_7/task_three.py
@@ -3,10 +3,12 @@ text2 = 'результат операции: 54'
 text3 = 'результат работы программы: 209'
 text4 = 'результат: 2'
 
+
 def add_ten(text):
     colon_index = text.index(':')
     result = int(text[colon_index + 1:].strip()) + 10
     print(result)
+
 
 add_ten(text1)
 add_ten(text2)
