709d344c8bafffeedbf843705e29c85c5740dc52
IrinaSli <ir.kasiakina@gmail.com>
2025-05-06 12:25:20 +0200

    homework 13 is done

---
+++ homework/irina_sliborskaya/Homework_13/files.py
@@ -0,0 +1,23 @@
+import datetime
+
+dates = []
+
+def read_file():
+    file_path = "D:\\Autotesting\\Python\\Project\\sliborskayaiv\\homework\\eugene_okulik\\hw_13\\data.txt"
+    with open(file_path, 'r', encoding='utf-8') as data_file:
+        for line in data_file.readlines():
+            yield line
+
+for data_line in read_file():
+    split_text = data_line.split(' - ')
+    new_data = split_text[0].split('. ')[1]
+    dates.append(new_data)
+
+date1 = datetime.datetime.strptime(dates[0], "%Y-%m-%d %H:%M:%S.%f")
+date2 = datetime.datetime.strptime(dates[1], "%Y-%m-%d %H:%M:%S.%f")
+date3 = datetime.datetime.strptime(dates[2], "%Y-%m-%d %H:%M:%S.%f")
+now = datetime.datetime.now()
+
+print(date1 + datetime.timedelta(weeks=1))
+print(date2.strftime('%A'))
+print((now - date3).days)
