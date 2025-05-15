710dfe5073e854e8147b7da02e36113ff34ddeac
IrinaSli <ir.kasiakina@gmail.com>
2025-05-15 10:27:58 +0200

    changes in homework 17

--- homework/irina_sliborskaya/Homework_17/analyzer.py
+++ homework/irina_sliborskaya/Homework_17/analyzer.py
@@ -1,10 +1,10 @@
 import argparse
-import os.path
+import os
 
 
 def log_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("file_path", help="Enter path to logs file")
+    parser.add_argument("folder_path", help="Enter path to logs folder")
     parser.add_argument("--text", help="Enter what to search")
     args = parser.parse_args()
 
@@ -12,20 +12,23 @@ def log_parser():
         print("No text provided for search.")
         return
 
-    try:
-        with open(args.file_path, 'r', encoding='utf-8') as file:
-            for i, line in enumerate(file, start=1):
-                words = line.strip().split()
-                file_name = os.path.basename(args.file_path).split('\\')[-1]
-                if args.text in line:
-                    for position, word in enumerate(words):
-                        if args.text in word:
-                            start = max(position - 5, 0)
-                            end = position + 6
-                            context = words[start:end]
-                            print(f"File {file_name}. Line {i}: {' '.join(context)}")
-    except FileNotFoundError:
-        print("No such file")
+    for filename in os.listdir(args.folder_path):
+        if filename:
+            file_path = os.path.join(args.folder_path, filename)
+            try:
+                with open(file_path, 'r', encoding='utf-8') as file:
+                    for i, line in enumerate(file, start=1):
+                        words = line.strip().split()
+                        file_name = os.path.basename(file_path).split('\\')[-1]
+                        if args.text in line:
+                            for position, word in enumerate(words):
+                                if args.text in word:
+                                    start = max(position - 5, 0)
+                                    end = position + 6
+                                    context = words[start:end]
+                                    print(f"File {file_name}. Line {i}: {' '.join(context)}")
+            except FileNotFoundError:
+                print("No such file")
 
 
 if __name__ == "__main__":
